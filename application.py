8dc56b46e6bfc3d0cf4586baf6dd85b95dcc93f3
dmitryh <dmitryh@comita.ru>
2017-11-30 15:54:38 +0300

    добавлен application.py

---
+++ application.py
@@ -0,0 +1,63 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+__author__ = 'dmitryh'
+
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.maximize_window()
+        self.wd.implicitly_wait(2)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://nadzor.comita.lan:8080/watch/")
+
+    def login_admin(self):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_id("loginBtn").click()
+        #WebDriverWait(wd, 3).until(EC.presence_of_element_located((By.XPATH, ".//*[@id='tab1']/infotable/div/div[2]/table/tbody/tr[1]/td[2]/div")))
+        wd.find_element_by_id("password").send_keys("123")
+        wd.find_element_by_id("username").send_keys("admin")
+        #WebDriverWait(wd, 3).until(EC.text_to_be_present_in_element_value(By.XPATH, '//*[@id="username" and text() = "admin"]'))
+        wd.find_element_by_id("loginDlgaction_saveBtn").click()
+
+    def user_properties_open(self):
+        wd = self.wd
+        wd.find_element_by_xpath("//*[@id='tab0']/userstable/div[2]/div[2]/table/tbody/tr[1]/td[1]").click()
+        wd.find_element_by_xpath("//div[@id='tab0']/userstable/div[1]/div/div[2]").click()
+
+    def expand_module_zivs(self):
+        wd = self.wd
+        # expand field Модули ЗИВС
+        wd.find_element_by_xpath("//div[@class='ms-options-wrap']/button").click()
+        # wait form loaded
+        WebDriverWait(wd, 5).until(EC.presence_of_element_located((By.XPATH, "//label[@for='ms-opt-14']")))
+
+    def select_sao321(self):
+        wd = self.wd
+        # select САО 321
+        wd.find_element_by_xpath("//label[@for='ms-opt-14']").click()
+        if wd.find_element_by_id("ms-opt-14").is_selected():
+            wd.find_element_by_id("ms-opt-14").click()
+
+    def save_properties(self):
+        wd = self.wd
+        wd.find_element_by_id("userListaction_saveBtn").click()
+        # wait to save
+        WebDriverWait(wd, 2).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, ".ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only")))
+        # click close Информация сохранена
+        wd.find_element_by_css_selector(
+            ".ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only").click()
+
+    def destroy(self):
+        self.wd.quit()
+
+
+
+
